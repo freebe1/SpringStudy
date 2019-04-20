3c192089c61e2a50e78ebbf55518b2b9fe6537c4
freebe1 <47933967+freebe1@users.noreply.github.com>
2019-04-21 03:19:34 +0900

    Servlet Test

---
+++ servletTest/src/org/sevTest/HelloServlet.java
@@ -0,0 +1,49 @@
+package org.sevTest;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet({ "/HelloServlet", "/hello", "*.do" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// request = Browser에서 Tomcat으로 정보 보냄
+		// response = Tomcat에서 Browser
+		System.out.println("Hi");
+		response.setHeader("Content-Type", "text/html"); // "text/html" "text/plain" 변경가능
+		PrintWriter out = response.getWriter();
+		//out.append("Served at: ").append(request.getContextPath());
+		out.write("<h1>Hello~</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
